3dd25d5e812d97978d0eb08ea240360f1d4d4afc
Anderson Belderrama <andersonbelderrama@gmail.com>
2024-02-20 23:03:28 -0300

    commit inicial

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar :elevation="2">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+    <v-app-bar-title>Desafio PRODEMGE</v-app-bar-title>
+
+    <template v-slot:append>
+      <v-btn icon="mdi-heart"></v-btn>
+
+      <v-btn icon="mdi-magnify"></v-btn>
+
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </template>
+  </v-app-bar>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+/**
+ * router/index.ts
+ *
+ * Automatic routes for `./src/pages/*.vue`
+ */
+
+// Composables
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { setupLayouts } from 'virtual:generated-layouts'
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  extendRoutes: (routes) => setupLayouts(routes),
+})
+
+export default router
